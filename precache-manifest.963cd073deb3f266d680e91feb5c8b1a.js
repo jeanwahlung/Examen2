c6f6246c5ce8b83663f1e67cd8999bee8020dfa5
jeanwahlung <jeancarloswahlung@gmail.com>
2019-03-21 13:19:15 -0600

    Updates

---
+++ precache-manifest.963cd073deb3f266d680e91feb5c8b1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6b9e7e75fe2b4f61fcd",
+    "url": "/Examen2/static/js/runtime~main.d7b62e55.js"
+  },
+  {
+    "revision": "47011400daf1ce1be266",
+    "url": "/Examen2/static/js/main.aaa68c91.chunk.js"
+  },
+  {
+    "revision": "3a9523b0f23235b7c479",
+    "url": "/Examen2/static/js/2.a0a22fb9.chunk.js"
+  },
+  {
+    "revision": "47011400daf1ce1be266",
+    "url": "/Examen2/static/css/main.8781f7f7.chunk.css"
+  },
+  {
+    "revision": "9974645c17be5ee63f961bba99cdb10d",
+    "url": "/Examen2/index.html"
+  }
+];
